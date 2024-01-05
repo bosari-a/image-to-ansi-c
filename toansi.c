181f6f686e7e01ce23b251549c09913e339d5508
bosari-a <ahmedelbosarimc@gmail.com>
2024-01-05 14:31:27 +0400

    setup for conversion and resize to bmp complete

--- toansi.c
+++ toansi.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "bmp.h"
-
+// ansi escap codes for command-line messages
 #define RED "\x1b[31m"
 #define GREEN "\x1b[32m"
 #define BLUE "\x1b[34m"
 #define YELLOW "\x1b[33m"
 #define RESET "\x1b[0m"
+// resize image in argument
+#define RESIZE 24
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    /**
+     * This section sets up the BMP 3.x format image
+     * argument (can be any image, e.g.  png, jpeg, etc...)
+     * is converted to BMP3 and resized to size RESIZE
+     */
+
+    // check correct usage
+    if (argc != 3 || (strcmp(argv[2], "ansii") != 0 && strcmp(argv[2], "html") != 0))
     {
-        printf(RED "\tIncorrect usage\n" RESET);
-        printf(YELLOW "\tUsage: " RESET);
+        printf(RED "Incorrect usage\n" RESET);
+        printf(YELLOW "Usage: " RESET "toansi {path/to/image} option\n");
+        printf(BLUE "\tExample: toansi" GREEN " ./img.png " YELLOW "ansii\n" RESET);
+        printf("Available options: " YELLOW "[ansii] [html]\n" RESET);
+        exit(1);
     }
+
+    // create bmp conversion command
+    char to_bmp[1024];
+    sprintf(to_bmp, "bash ./process_img %s %i", argv[1], RESIZE);
+    // call command
+    system(to_bmp);
+
+    // open generated bmp file
 }
